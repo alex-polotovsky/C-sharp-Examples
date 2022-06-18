1eb0ff4c360acde8871cbfb31c3a20d63fe5b600
alexandr polotovsky <apolotovsky@gmail.com>
2022-06-18 18:24:52 +0300

    add cycle_for and selection_sort

---
+++ Example012(2)_MethodsMaxMin/Program.cs
@@ -0,0 +1,36 @@
+﻿// Сортировка одномерного массива.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = array.Length - 1; i > 0; i--)
+    {
+        int maxPosition = i;
+
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
